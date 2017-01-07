1e2573054a753a5b210345bc2801398bcdb5e2c1
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-01-07 14:30:09 +0900

    Add lisence lines

--- scripts/voice_to_command1.py
+++ scripts/voice_to_command1.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 #encoding: utf8
+
+#motors.py
+#Copyright (c) 2016 Ryuichi Ueda <ryuichiueda@gmail.com>
+#This software is released under the MIT License.
+#http://opensource.org/licenses/mit-license.php
+
 import rospy, os, socket
 
 class JuliusReceiver:
